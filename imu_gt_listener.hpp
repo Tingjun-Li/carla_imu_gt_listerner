3a4ea228561c6d78ae455f5f8518d32de1c6d930
Tingjun Li <tingjunl@umich.edu>
2022-02-25 15:09:29 -0500

    Added velocity publisher

--- imu_gt_listener.hpp
+++ imu_gt_listener.hpp
@@ -12,6 +12,7 @@
 // #include "geometry_msgs/PoseWithCovarianceStamped.h"
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/Point.h>
+#include <geometry_msgs/TwistStamped.h>
 #include <geometry_msgs/Quaternion.h>
 #include <geometry_msgs/PoseStamped.h>
 #include "nav_msgs/Odometry.h"
@@ -20,20 +21,24 @@
 #include "geometry_msgs/PoseWithCovarianceStamped.h"
 #include <nav_msgs/Path.h>
 #include <random>
+#include "carla_msgs/CarlaEgoVehicleStatus.h"
 
 class CarlaListener {
 public:
   CarlaListener(ros::NodeHandle& nh): nh_(nh),
                                     gyro_noise(0.0515698903002, 0.037322230667),
                                     acc_noise(0.515757598072, 0.654051323977) {
-    imu_pub = nh_.advertise<sensor_msgs::Imu>("imu_with_noise", 1000);
-    path_pub = nh_.advertise<nav_msgs::Path>("gt_path", 1000);
     
     imu_sub = nh.subscribe("/carla/ego_vehicle/imu", 1000, &CarlaListener::ImuCallback, this);
     gt_sub = nh.subscribe("/carla/ego_vehicle/odometry", 1000, &CarlaListener::OdomCallback, this);
+    velocity_sub = nh.subscribe("/carla/ego_vehicle/vehicle_status", 1000, &CarlaListener::VelocityCallback, this);
+
+    imu_pub = nh_.advertise<sensor_msgs::Imu>("imu_with_noise", 1000);
+    path_pub = nh_.advertise<nav_msgs::Path>("gt_path", 1000);
+    velocity_pub = nh_.advertise<geometry_msgs::TwistStamped>("carla_velocity", 1000);
   }
 
-  ~CarlaListener() {}
+  // ~CarlaListener() {}
 
   void ImuCallback(const sensor_msgs::Imu& imu_msg)
   {
@@ -43,14 +48,15 @@ public:
     imu_with_noise_msg.orientation = imu_msg.orientation;
     imu_with_noise_msg.angular_velocity = imu_msg.angular_velocity;
 
-    imu_with_noise_msg.angular_velocity.x += gyro_noise(generator);
-    imu_with_noise_msg.angular_velocity.y += gyro_noise(generator);
-    imu_with_noise_msg.angular_velocity.z += gyro_noise(generator);
+    int noise_factor = 1;
+    imu_with_noise_msg.angular_velocity.x += noise_factor * gyro_noise(generator);
+    imu_with_noise_msg.angular_velocity.y += noise_factor * gyro_noise(generator);
+    imu_with_noise_msg.angular_velocity.z += noise_factor * gyro_noise(generator);
 
     imu_with_noise_msg.linear_acceleration = imu_msg.linear_acceleration;
-    imu_with_noise_msg.linear_acceleration.x += acc_noise(generator);
-    imu_with_noise_msg.linear_acceleration.y += acc_noise(generator);
-    imu_with_noise_msg.linear_acceleration.z += acc_noise(generator);
+    imu_with_noise_msg.linear_acceleration.x += noise_factor * acc_noise(generator);
+    imu_with_noise_msg.linear_acceleration.y += noise_factor * acc_noise(generator);
+    imu_with_noise_msg.linear_acceleration.z += noise_factor * acc_noise(generator);
 
     imu_with_noise_msg.header = imu_msg.header;
     imu_pub.publish(imu_with_noise_msg);
@@ -84,13 +90,26 @@ public:
     }
   }
 
+  void VelocityCallback(const carla_msgs::CarlaEgoVehicleStatus& msg)
+  {
+    geometry_msgs::TwistStamped velocity_msg;
+    velocity_msg.header = msg.header;
+    velocity_msg.twist.linear.x = msg.velocity;
+    velocity_pub.publish(velocity_msg);
+    
+  }
+
 private:
     ros::NodeHandle& nh_;
     std::vector<geometry_msgs::PoseStamped> poses_;
     ros::Subscriber imu_sub;
     ros::Subscriber gt_sub;
+    ros::Subscriber velocity_sub;
+
     ros::Publisher imu_pub;
     ros::Publisher path_pub;
+    ros::Publisher velocity_pub;
+
     std::default_random_engine generator;
     std::normal_distribution<double> gyro_noise;
     std::normal_distribution<double> acc_noise;
